5ee2e804fcc2835dcfc50ea9d321190a4175536f
marinanik97 <nikolicmarina555@gmail.com>
2020-04-30 21:50:39 +0200

    Adding signup screen

--- navigation/MainStackNavigator.js
+++ navigation/MainStackNavigator.js
@@ -1,75 +1,19 @@
 import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+const Stack = createStackNavigator()
 import LogInScreen from "../screens/LogInScreen";
 import UsersScreen from "../screens/UsersScreen";
-import {useContext} from "react";
-import {Context as AuthContext} from "../context/AuthContext";
-import LinksScreen from "../screens/LinksScreen";
-import HomeScreen from "../screens/HomeScreen";
 
-const RootStack = createStackNavigator();
-
-const AuthStack = createStackNavigator();
-const HomeStack = createStackNavigator();
-const LinkStack = createStackNavigator();
-const UserStack = createStackNavigator();
-const Tabs = createBottomTabNavigator();
-
-const AuthStackScreen = () => (
-    <AuthStack.Navigator>
-        <AuthStack.Screen
-            name="LogIn"
-            component={LogInScreen}
-            options={{ title: "Log in" }}
-        />
-    </AuthStack.Navigator>
-);
-
-const HomeStackScreen = () => (
-    <HomeStack.Navigator>
-        <HomeStack.Screen name="Home" component={HomeScreen} />
-        <HomeStack.Screen name="Users" component={UsersScreen} />
-        <HomeStack.Screen name="Links" component={LinksScreen}/>
-    </HomeStack.Navigator>
-);
-
-const LinkStackScreen = () => (
-    <LinkStack.Navigator>
-        <LinkStack.Screen name="Links" component={LinksScreen}/>
-        <LinkStack.Screen name="Users" component={UsersScreen} />
-        <LinkStack.Screen name="Home" component={HomeScreen} />
-    </LinkStack.Navigator>
-);
-
-const UserStackScreen = () => (
-    <UserStack.Navigator>
-        <UserStack.Screen name="Users" component={UsersScreen} />
-        <UserStack.Screen name="Home" component={HomeScreen} />
-        <UserStack.Screen name="Links" component={LinksScreen}/>
-    </UserStack.Navigator>
-);
-
-const TabsScreen = () => (
-    <Tabs.Navigator>
-        <Tabs.Screen name="Home" component={HomeStackScreen} />
-        <Tabs.Screen name="Link" component={LinkStackScreen} />
-        <Tabs.Screen name="User" component={UserStackScreen} />
-    </Tabs.Navigator>
-);
-
-export default function RootStackNavigator() {
-    const {state, LogIn} = useContext(AuthContext)
-
+function MainStackNavigator() {
     return (
         <NavigationContainer>
-            <RootStack.Navigator headerMode="none">
-                {!state.token &&
-                <RootStack.Screen name='Login' component={AuthStackScreen} />
-                }
-                <RootStack.Screen name='Users' component={TabsScreen} />
-            </RootStack.Navigator>
+            <Stack.Navigator>
+                <Stack.Screen name='Login' component={LogInScreen} />
+                <Stack.Screen name='Users' component={UsersScreen} />
+            </Stack.Navigator>
         </NavigationContainer>
     )
 }
+
+export default MainStackNavigator
