b9677307caf83de696bcda73270fd7d0714be7db
igormirkoviccc <igor.mirkovic@enetelsolutions.com>
2020-05-05 23:31:19 +0200

    adding icons in footer menu

--- navigation/MainStackNavigator.js
+++ navigation/MainStackNavigator.js
@@ -1,12 +1,12 @@
 import * as React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import LogInScreen from "../screens/LogInScreen";
 import NewsFeedScreen from "../screens/NewsFeedScreen";
 import {useContext, useState, useEffect} from "react";
 import {AsyncStorage, Text, View} from "react-native";
-import { Button } from 'react-native-elements';
+import {Button} from 'react-native-elements';
 
 import {Context as AuthContext} from "../context/AuthContext";
 import LinksScreen from "../screens/LinksScreen";
@@ -15,16 +15,18 @@ import SignUpScreen from "../screens/SignUpScreen";
 import {navigationRef} from "./RootNavigation";
 import NewPostScreen from "../screens/NewPostScreen";
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import ExploreScreen from "../screens/ExploreScreen";
+import ChatScreen from "../screens/ChatScreen";
 
 
 const RootStack = createStackNavigator();
 const AuthStack = createStackNavigator();
 const ProfileStack = createStackNavigator();
-const LinkStack = createStackNavigator();
+const ExploreStack = createStackNavigator();
 const NewsFeedStack = createStackNavigator();
 const Tabs = createBottomTabNavigator();
 
-const LogOut = async () =>{
+const LogOut = async () => {
     await AsyncStorage.removeItem('auth_token');
     await AsyncStorage.removeItem('id');
 }
@@ -35,12 +37,12 @@ const AuthStackScreen = () => (
         <AuthStack.Screen
             name="LogIn"
             component={LogInScreen}
-            options={{ title: "Log in" }}
+            options={{title: "Log in"}}
         />
         <AuthStack.Screen
             name="SignUp"
             component={SignUpScreen}
-            options={{ title: "Sign up" }}
+            options={{title: "Sign up"}}
         />
     </AuthStack.Navigator>
 );
@@ -63,41 +65,42 @@ const ProfileStackScreen = ({navigation}) => {
                     ),
                 }}
             />
-            <ProfileStack.Screen name="News feed"
-                              component={NewsFeedScreen} />
-            <ProfileStack.Screen name="Links" component={LinksScreen}/>
+            <ProfileStack.Screen name="News feed" component={NewsFeedScreen}/>
+            <ProfileStack.Screen name="Explore" component={ExploreScreen}/>
             <ProfileStack.Screen name="New Post" component={NewPostScreen}/>
 
         </ProfileStack.Navigator>
     );
 }
 
-const LinkStackScreen = () => (
-    <LinkStack.Navigator>
-        <LinkStack.Screen name="Links" component={LinksScreen}/>
-        <LinkStack.Screen name="News feed" component={NewsFeedScreen} />
-        <LinkStack.Screen name="Profile" component={ProfileScreen} />
-    </LinkStack.Navigator>
+const ExploreStackScreen = () => (
+    <ExploreStack.Navigator>
+        <ExploreStack.Screen name="Explore" component={ExploreScreen}/>
+        <ExploreStack.Screen name="News feed" component={NewsFeedScreen}/>
+        <ExploreStack.Screen name="Profile" component={ProfileScreen}/>
+    </ExploreStack.Navigator>
 );
 
 const NewsFeedStackScreen = ({navigation}) => (
     <NewsFeedStack.Navigator>
         <NewsFeedStack.Screen name="News feed"
-                          options={{
-                              headerRight: () => (
-                                  <Button
-                                      onPress={() => navigation.navigate('New Post')}
-                                      type='outline'
-                                      title="Add post"
-                                      color="#000"
-                                      buttonStyle={{height: 30, marginRight: 10}}
-                                      titleStyle={{fontSize: 12}}
-                                  />
-                              ),
-                          }} component={NewsFeedScreen} />
-        <NewsFeedStack.Screen name="Profile" component={ProfileScreen} />
-        <NewsFeedStack.Screen name="New Post" component={NewPostScreen} />
-        <NewsFeedStack.Screen name="Links" component={LinksScreen}/>
+                              options={{
+                                  headerRight: () => (
+                                      <Button
+                                          onPress={() => navigation.navigate('New Post')}
+                                          type='outline'
+                                          title="Add post"
+                                          color="#000"
+                                          buttonStyle={{height: 30, marginRight: 10}}
+                                          titleStyle={{fontSize: 12}}
+                                      />
+                                  ),
+                              }} component={NewsFeedScreen}/>
+        <NewsFeedStack.Screen name="Profile" component={ProfileScreen}/>
+        <NewsFeedStack.Screen name="New Post" component={NewPostScreen}/>
+        <NewsFeedStack.Screen name="Explore" component={ExploreScreen}/>
+        <NewsFeedStack.Screen name="Chat Screen" component={ChatScreen}/>
+
     </NewsFeedStack.Navigator>
 );
 
@@ -108,29 +111,29 @@ const TabsScreen = () => (
                          tabBarIcon: () =>
                              <Icon name='newspaper' color='black' size={20}/>
                      }}
-                     component={NewsFeedStackScreen} />
+                     component={NewsFeedStackScreen}/>
         <Tabs.Screen
             name="Profile"
             options={{
                 tabBarIcon: () =>
                     <Icon name='face-profile' color='black' size={20}/>
             }}
-            component={ProfileScreen} />
+            component={ProfileScreen}/>
         <Tabs.Screen
-            name="Chat"
+            name="Explore"
             options={{
                 tabBarIcon: () =>
-                    <Icon name='chat' color='black' size={20}/>
+                    <Icon name='magnify' color='black' size={20}/>
             }}
-            component={LinkStackScreen} />
+            component={ExploreStackScreen}/>
     </Tabs.Navigator>
 );
 
 export default function RootStackNavigator() {
-    const { state } = useContext(AuthContext)
+    const {state} = useContext(AuthContext)
     const [isLoading, setLoading] = useState(true);
 
-    const GetFromAsyncStorage = async () =>{
+    const GetFromAsyncStorage = async () => {
         await AsyncStorage.getItem('auth_token').then((token) => {
             state.isAuth = !!token;
         });
@@ -140,12 +143,12 @@ export default function RootStackNavigator() {
     }
 
     useEffect(async () => {
-         await GetFromAsyncStorage();
-         setLoading(false);
+        await GetFromAsyncStorage();
+        setLoading(false);
 
     }, [])
 
-    if(isLoading){
+    if (isLoading) {
         return <View>
             <Text>Loading...</Text>
         </View>
@@ -156,9 +159,9 @@ export default function RootStackNavigator() {
         <NavigationContainer ref={navigationRef}>
             <RootStack.Navigator headerMode="none">
                 {!state.isAuth &&
-                <RootStack.Screen name='Auth' component={AuthStackScreen} />
+                <RootStack.Screen name='Auth' component={AuthStackScreen}/>
                 }
-                <RootStack.Screen name='News feed' component={TabsScreen} />
+                <RootStack.Screen name='News feed' component={TabsScreen}/>
             </RootStack.Navigator>
         </NavigationContainer>
     )
---
+++ screens/ChatScreen.js
@@ -0,0 +1,120 @@
+import React, {useContext, useEffect, useState} from "react";
+import {AsyncStorage, ScrollView, StyleSheet, View, Image, Text} from 'react-native';
+import Post from '../components/Post'
+import {Context as AuthContext} from "../context/AuthContext";
+import TextInput from "react-native-web/src/exports/TextInput";
+import {Button} from "react-native-elements";
+import Message from "../components/Message";
+
+
+export default function ChatScreen({navigation, route}) {
+    const {state, LogIn} = useContext(AuthContext)
+    const [user, setUser] = useState({...route.params?.user})
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+
+    useEffect( () =>{
+         return navigation.addListener('focus', () => {
+            getMessages();
+        });
+
+    }, [navigation])
+
+    console.log(messages);
+
+    const sendMessage = async () => {
+        const messageObj = {
+            text: message,
+            user: user._id
+        }
+        const token = await AsyncStorage.getItem('auth_token');
+        fetch('http://159.65.165.71:8000/messages/new', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization' : token
+            },
+            body: JSON.stringify(messageObj)
+        })
+            .then((res) => {
+                if(res.ok){
+                    console.log('sent');
+                    setMessage('');
+                }else{
+                    console.log(res);
+                }
+            })
+            .catch((e) => {
+                console.log(e)
+            })
+    }
+
+    const getMessages = async () =>{
+        const token = await AsyncStorage.getItem('auth_token');
+        setInterval( () =>{
+            fetch('http://159.65.165.71:8000/messages/' +user._id, {
+                headers: {
+                    'Authorization' : token
+                }})
+                .then(res => res.json())
+                .then(res => setMessages(res))
+        }, 1000)
+    }
+
+    const renderMessages = () =>{
+        if(messages){
+            return messages.map((message) =>{
+                return <Message message={message}/>
+            })
+        }
+    }
+
+    return(
+        <ScrollView style={styles.container}>
+            <View style={styles.labelname}>
+                <Text>{user.name}</Text>
+            </View>
+            <ScrollView style={styles.messagesContainer}>
+                {renderMessages()}
+            </ScrollView>
+            <View style={{borderWidth: 1, borderColor: '#F1F2EB', marginTop: 50}}>
+                <TextInput onChangeText={(text) => setMessage(text)}
+                           value={message} multiline={true} numberOfLines={8} style={styles.textInputContainer}/>
+            </View>
+            <View style={styles.actionContainer}>
+                <Button onPress={() => setMessage('')} type='outline' buttonStyle={{marginLeft: 10, color: 'red'}} title='Cancel'/>
+                <Button onPress={() => sendMessage()} type='outline' title='Apply'/>
+            </View>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'white',
+        padding: 30,
+    },
+    labelname: {
+        width: '100%',
+        textAlign: 'center',
+        marginBottom: 20
+    },
+    messagesContainer: {
+        maxHeight: 400,
+        width: '100%',
+        flex:1,
+    },
+    textInputContainer: {
+        borderWidth: 1, textAlignVertical: 'top', borderColor: '#F1F2EB', padding: 20, borderRadius: 5
+    },
+    actionContainer:{
+        width: '100%',
+        height: 60,
+        display: 'flex',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        flexDirection: 'row-reverse'
+    }
+})
